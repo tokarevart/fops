3a64c9168461c5cfca237c911abbb12e18e267ee
Tokarev Artem <tokarev28.art@gmail.com>
2023-01-21 08:18:21 +0500

    remove static pubsub

--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,3 @@ pub use message::text_message::TextMessage;
 pub use pubsub::PubSub;
 pub use subscription::Subscription;
 pub use topic::Topic;
-
-static PUBSUB: PubSub<BinaryMessage> = PubSub::new();
-
-pub async fn topic(name: &[u8]) -> Topic<BinaryMessage> {
-    PUBSUB.topic(name).await
-}
-
-pub async fn topic_with_capacity(name: &[u8], capacity: usize) -> Topic<BinaryMessage> {
-    PUBSUB.topic_with_capacity(name, capacity).await
-}
--- tests/forps.rs
+++ tests/forps.rs
@@ -1,36 +1,46 @@
+use forps::{BinaryMessage, PubSub, TextMessage};
+
 #[tokio::test]
 async fn create_topic() {
+    let pubsub = PubSub::<BinaryMessage>::new();
+
     let topic_name = b"topic name";
-    let topic = forps::topic(topic_name).await;
+    let topic = pubsub.topic(topic_name).await;
     assert_eq!(topic.name(), topic_name);
 }
 
 #[tokio::test]
 async fn create_two_topics() {
+    let pubsub = PubSub::<BinaryMessage>::new();
+
     let topic_name = b"topic name";
-    let _ = forps::topic(topic_name).await;
+    let _ = pubsub.topic(topic_name).await;
     let topic_name = b"another topic name";
-    let topic = forps::topic(topic_name).await;
+    let topic = pubsub.topic(topic_name).await;
     assert_eq!(topic.name(), topic_name);
 }
 
 #[tokio::test]
 async fn get_existing_topic() {
+    let pubsub = PubSub::<BinaryMessage>::new();
+
     let topic_name = b"topic name";
-    let _ = forps::topic(topic_name).await;
-    let topic = forps::topic(topic_name).await;
+    let _ = pubsub.topic(topic_name).await;
+    let topic = pubsub.topic(topic_name).await;
     assert_eq!(topic.name(), topic_name);
 }
 
 #[tokio::test]
 async fn publish_and_receive_binary() {
+    let pubsub = PubSub::new();
+
     let topic_name = b"topic name";
-    let topic = forps::topic(topic_name).await;
+    let topic = pubsub.topic(topic_name).await;
 
     let mut subscription = topic.subscribe();
     let handle = tokio::spawn(async move { subscription.receive().await });
 
-    let msg = forps::BinaryMessage::from(b"message");
+    let msg = BinaryMessage::from(b"message");
     topic.publish(msg.clone()).unwrap();
 
     let recv_msg = handle.await.unwrap().unwrap();
@@ -39,7 +49,7 @@ async fn publish_and_receive_binary() {
 
 #[tokio::test]
 async fn publish_and_receive_text() {
-    let pubsub = forps::PubSub::new();
+    let pubsub = PubSub::new();
 
     let topic_name = b"topic name";
     let topic = pubsub.topic(topic_name).await;
@@ -47,7 +57,7 @@ async fn publish_and_receive_text() {
     let mut subscription = topic.subscribe();
     let handle = tokio::spawn(async move { subscription.receive().await });
 
-    let msg = forps::TextMessage::from("message");
+    let msg = TextMessage::from("message");
     topic.publish(msg.clone()).unwrap();
 
     let recv_msg = handle.await.unwrap().unwrap();
