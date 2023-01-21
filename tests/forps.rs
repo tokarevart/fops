7214fcadee51073ea5ac6fa9803cb6a697f5d48e
Tokarev Artem <tokarev28.art@gmail.com>
2023-01-21 07:56:51 +0500

    rename crate

--- tests/forps.rs
+++ tests/forps.rs
@@ -1,36 +1,36 @@
 #[tokio::test]
 async fn create_topic() {
     let topic_name = b"topic name";
-    let topic = glops::topic(topic_name).await;
+    let topic = forps::topic(topic_name).await;
     assert_eq!(topic.name(), topic_name);
 }
 
 #[tokio::test]
 async fn create_two_topics() {
     let topic_name = b"topic name";
-    let _ = glops::topic(topic_name).await;
+    let _ = forps::topic(topic_name).await;
     let topic_name = b"another topic name";
-    let topic = glops::topic(topic_name).await;
+    let topic = forps::topic(topic_name).await;
     assert_eq!(topic.name(), topic_name);
 }
 
 #[tokio::test]
 async fn get_existing_topic() {
     let topic_name = b"topic name";
-    let _ = glops::topic(topic_name).await;
-    let topic = glops::topic(topic_name).await;
+    let _ = forps::topic(topic_name).await;
+    let topic = forps::topic(topic_name).await;
     assert_eq!(topic.name(), topic_name);
 }
 
 #[tokio::test]
 async fn publish_and_receive_binary() {
     let topic_name = b"topic name";
-    let topic = glops::topic(topic_name).await;
+    let topic = forps::topic(topic_name).await;
 
     let mut subscription = topic.subscribe();
     let handle = tokio::spawn(async move { subscription.receive().await });
 
-    let msg = glops::BinaryMessage::from(b"message");
+    let msg = forps::BinaryMessage::from(b"message");
     topic.publish(msg.clone()).unwrap();
 
     let recv_msg = handle.await.unwrap().unwrap();
@@ -39,7 +39,7 @@ async fn publish_and_receive_binary() {
 
 #[tokio::test]
 async fn publish_and_receive_text() {
-    let pubsub = glops::PubSub::new();
+    let pubsub = forps::PubSub::new();
 
     let topic_name = b"topic name";
     let topic = pubsub.topic(topic_name).await;
@@ -47,7 +47,7 @@ async fn publish_and_receive_text() {
     let mut subscription = topic.subscribe();
     let handle = tokio::spawn(async move { subscription.receive().await });
 
-    let msg = glops::TextMessage::from("message");
+    let msg = forps::TextMessage::from("message");
     topic.publish(msg.clone()).unwrap();
 
     let recv_msg = handle.await.unwrap().unwrap();
